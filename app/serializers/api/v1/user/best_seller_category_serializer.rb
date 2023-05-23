9806f47c846854d1476225d2328ab61a1d08fdd1
Gavin Moh <8167652+gavinmoh@users.noreply.github.com>
2023-05-23 13:42:53 +0800

    Fix best seller category products serializer

--- app/serializers/api/v1/user/best_seller_category_serializer.rb
+++ app/serializers/api/v1/user/best_seller_category_serializer.rb
@@ -2,7 +2,7 @@ class Api::V1::User::BestSellerCategorySerializer < ActiveModel::Serializer
   attributes :id, :name, :display_order, :photo
   attributes :created_at, :updated_at
 
-  has_many :products, each_serializer: Api::V1::User::BestSellerProductSerializer do |serializer|
+  has_many :products, serializer: Api::V1::User::BestSellerProductSerializer do |serializer|
     serializer.send(:instance_options)[:products].select { |product| product.category_id == object.id }
   end
 end
