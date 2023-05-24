02767c70c5a16554f2c0984b1b4cf1c64266dbf9
Gavin Moh <8167652+gavinmoh@users.noreply.github.com>
2023-05-24 14:24:45 +0800

    Added Inventory and InventoryTransaction spec

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,4 +3,6 @@ class Inventory < ApplicationRecord
   belongs_to :product, class_name: 'BaseProduct'
 
   has_many :inventory_transactions, dependent: :destroy
+
+  validates :quantity, numericality: { only_integer: true }, allow_nil: false
 end
--- spec/models/inventory_transaction_spec.rb
+++ spec/models/inventory_transaction_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe InventoryTransaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:order).optional }
+    it { should belong_to(:inventory) }
+  end
+
+  describe 'validations' do
+    it { should validate_numericality_of(:quantity).is_other_than(0).only_integer }
+  end
+
+  describe 'callbacks' do
+    describe 'after_commit' do
+      it 'update inventory quantity' do
+        inventory = create(:inventory)
+        transaction = create(:inventory_transaction, inventory: inventory, quantity: 10)
+        create(:inventory_transaction, inventory: inventory, quantity: 20)
+        inventory.reload
+        expect(inventory.quantity).to eq(30)
+
+        transaction.destroy
+        inventory.reload
+        expect(inventory.quantity).to eq(20)
+      end
+    end
+  end
 end
