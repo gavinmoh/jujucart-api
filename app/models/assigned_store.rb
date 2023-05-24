966e929fa7d15253bcda06e71efee1baae877219
Gavin Moh <8167652+gavinmoh@users.noreply.github.com>
2023-05-24 12:40:10 +0800

    Added AssignedStore spec

--- app/models/assigned_store.rb
+++ app/models/assigned_store.rb
@@ -2,5 +2,5 @@ class AssignedStore < ApplicationRecord
   belongs_to :user
   belongs_to :store
 
-  validates :user, uniqueness: { scope: :store }
+  validates :user, uniqueness: { scope: :store_id }
 end
--- spec/models/assigned_store_spec.rb
+++ spec/models/assigned_store_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe AssignedStore, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:store) }
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    subject { create(:assigned_store) }
+    it { should validate_uniqueness_of(:user).scoped_to(:store_id).case_insensitive }
+  end
 end
