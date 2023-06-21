249544742c532876310713ba8765cfc58ae7509f
Gavin Moh <gavin@atslabs.dev>
2023-06-21 12:06:31 +0800

    Added assigned_stores to Store serializer

--- app/serializers/api/v1/user/assigned_store_serializer.rb
+++ app/serializers/api/v1/user/assigned_store_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::User::AssignedStoreSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :store_id
   attributes :created_at, :updated_at
-  has_one :user
+  has_one :user, serializer: Api::V1::User::UserInfoSerializer
   has_one :store
 end
--- app/serializers/api/v1/user/store_serializer.rb
+++ app/serializers/api/v1/user/store_serializer.rb
@@ -2,6 +2,7 @@ class Api::V1::User::StoreSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :logo, :validate_inventory
   attributes :created_at, :updated_at
 
+  has_many :assigned_stores
   has_many :users, serializer: Api::V1::User::UserInfoSerializer
   has_many :pos_terminals
 end
