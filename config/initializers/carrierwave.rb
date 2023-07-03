0e8ee352eab651757aa8a69477a2d8a74d0f2118
Gavin Moh <gavin@atslabs.dev>
2023-07-03 15:23:48 +0800

    Updated carrierwave S3_ENDPOINT

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,12 +10,22 @@ CarrierWave.configure do |config|
     config.asset_host = ENV.fetch('ASSET_HOST', nil)
   else
     config.storage = :fog
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV.fetch('S3_KEY', nil),
-      aws_secret_access_key: ENV.fetch('S3_SECRET', nil),
-      region: ENV.fetch('S3_REGION', nil)
-    }
+    config.fog_credentials = if ENV['S3_ENDPOINT'].present?
+                               {
+                                 provider: 'AWS',
+                                 aws_access_key_id: ENV.fetch('S3_KEY', nil),
+                                 aws_secret_access_key: ENV.fetch('S3_SECRET', nil),
+                                 region: ENV.fetch('S3_REGION', nil),
+                                 endpoint: ENV['S3_ENDPOINT']
+                               }
+                             else
+                               {
+                                 provider: 'AWS',
+                                 aws_access_key_id: ENV.fetch('S3_KEY', nil),
+                                 aws_secret_access_key: ENV.fetch('S3_SECRET', nil),
+                                 region: ENV.fetch('S3_REGION', nil)
+                               }
+                             end
     config.fog_directory  = ENV.fetch('S3_BUCKET_NAME', nil)
     config.fog_public     = true
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
